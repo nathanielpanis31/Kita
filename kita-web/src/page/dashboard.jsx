0672dcbca1709314e9c1cadeacb58d40d02908b3
nathaniel <nathanielpanis31@gmail.com>
2026-04-08 20:40:55 +0800

    side bar

--- kita-web/src/page/dashboard.jsx
+++ kita-web/src/page/dashboard.jsx
@@ -4,7 +4,26 @@ function Dashboard() {
   return (
 
     <div className="dashboard">
-        <h1>Dashboard Page</h1>
+
+      {/* Dashboard Heading */}
+      <div className="dashboard-heading">
+
+        <div className="left-heading">
+          <h1>Dashboard</h1>
+          <p>Good Day!</p>
+        </div>
+
+        <div className="right-heading">
+          <button type="button" className="month">This month</button>
+          <button type="button" className="add-transaction"> + Add Transaction</button>
+        </div>  
+
+      </div> 
+
+      {/* Dashboard Content */}
+      <div className="dashboard-body">
+        <h1>This is the dashboard content.</h1>
+      </div>
     </div>
 
   )
